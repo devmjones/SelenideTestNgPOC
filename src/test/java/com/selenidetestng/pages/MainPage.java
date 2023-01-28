8ffe52ab21c0bc97cd89d2be9ce1ae9ea03a5464
devmjones <devmjones@gmail.com>
2023-01-28 10:11:53 -0700

    Clean Up

--- src/test/java/com/selenidetestng/pages/MainPage.java
+++ src/test/java/com/selenidetestng/pages/MainPage.java
@@ -2,44 +2,39 @@ package com.selenidetestng.pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.selector.ByText;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
-// page_url = https://www.jetbrains.com/
 public class MainPage {
     public SelenideElement seeDeveloperToolsButton = $x("//*[@data-test-marker='Developer Tools']");
     public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-action']");
-    public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
     public SelenideElement searchButton = $("[data-test='site-header-search-action']");
     public SelenideElement searchInput = $("input[data-test='search-input']");
-    public SelenideElement fullSearchBtn = $("button[data-test='full-search-button']");
     public SelenideElement searchBarShowingResultsForTitle = $("div.quick-search__results-title");
-    public SelenideElement searchBarResultsPanel =$("span.quick-search__results-query");
+    public SelenideElement searchBarResultsPanel = $("span.quick-search__results-query");
 
 
-    public void searchForText(String text){
+    public void searchForText(String text) {
         this.searchButton.click();
         this.searchInput.sendKeys(text);
         this.searchInput.shouldHave(attribute("value", text));
     }
 
-    public boolean searchResultPanelShowingCorrectTerm(String searchTerm){
+    public boolean searchResultPanelShowingCorrectTerm(String searchTerm) {
         this.searchBarShowingResultsForTitle.shouldHave(text(searchTerm));
         return this.searchBarResultsPanel.getText().equals(searchTerm);
 
     }
 
-    public void navigateToAllTools(){
-       this.seeDeveloperToolsButton.click();
-       this.findYourToolsButton.click();
-       $("#products-page").shouldBe(visible);
+    public void navigateToAllTools() {
+        this.seeDeveloperToolsButton.click();
+        this.findYourToolsButton.click();
+        $("#products-page").shouldBe(visible);
     }
 
-    public boolean onAllToolsPage (){
+    public boolean onAllToolsPage() {
         return Selenide.title().equals("All Developer Tools and Products by JetBrains");
     }
 }
--- src/test/java/com/selenidetestng/tests/MainPageTest.java
+++ src/test/java/com/selenidetestng/tests/MainPageTest.java
@@ -1,27 +1,21 @@
 package com.selenidetestng.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.selenidetestng.pages.MainPage;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.*;
 
 import static org.testng.Assert.*;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
-import static org.testng.Assert.assertEquals;
+
 
 public class MainPageTest {
     MainPage mainPage = new MainPage();
 
     @DataProvider(name = "search-terms")
     public Object[][] dataProvFunc() {
-        return new Object[][]{
-                {"Selenium"}, {"TestNg"}
-        };
+        return new Object[][]{{"Selenium"}, {"TestNg"}};
     }
 
     @BeforeClass(alwaysRun = true)
